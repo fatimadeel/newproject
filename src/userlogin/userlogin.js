f2a8c16206dcf1b3ba8fdddf95cc44194203bd3f
fatimadeel <fatimadeel85@gmail.com>
2019-02-18 15:17:45 +0100

    18th feb (REACT PROJECT)

--- src/userlogin/userlogin.js
+++ src/userlogin/userlogin.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 const Login = function ()
     {
-           return (
+        return (
            <div className = "loginpage">
-           <h4>User Name</h4>
-           <h4>Password</h4>
+           <h4>User Name:</h4>
+           <h4>Password:</h4>
            </div>)
     };
 
