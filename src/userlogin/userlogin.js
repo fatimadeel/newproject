c2ffa24b15e4ac40ae873bac447a98d3c9edb75b
fatimadeel <fatimadeel85@gmail.com>
2019-02-18 15:13:19 +0100

    ReactProject 18 Feb

---
+++ src/userlogin/userlogin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Login = function ()
+    {
+           return (
+           <div className = "loginpage">
+           <h4>User Name</h4>
+           <h4>Password</h4>
+           </div>)
+    };
+
+    export default Login;
