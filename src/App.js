d848412c81ba98a554e6c22857071c6aeab0a75b
fatimadeel <fatimadeel85@gmail.com>
2019-02-19 15:51:34 +0100

    Feb 19th REACT

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './userpage/userlogin';
+
+import Logga from './userpage/userlogin';
+
+// import img from './Header.png';
+// import img1 from './footer.png';
+
 
 class App extends Component {
+
   render() {
     return (
-      <Login />
+           <div>
+           <img src = {require('./Header.png')} />
+           <Logga />
+           <img src = {require('./footer.png')} />
+           </div>
     );
 
     // return React.createElement ('div', {className:'App'}, React.createElement('h1', null, 'Does it works well now?'))
--- src/userpage/userlogin.js
+++ src/userpage/userlogin.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './userlogin.css';
 
-const Login = function ()
-    {
-        return (
-           <div className = "loginpage">
-           <h4>User Name:</h4>
-           <h4>Password:</h4>
-           </div>)
-    };
+class Logga extends Component {
+         
+         loggaInHandler = function ()
+          {
+            console.log('Was clicked');
+          }
 
-    export default Login;
+     render ()
+       {
+        return (
+        <div className = "loginpage">
+         <label htmlFor="email">EMail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label> 
+         <input type="text" name="email" id="email" /> <br/>
+
+         <label htmlFor="password">Password:&nbsp;&nbsp;&nbsp;</label> 
+         <input type="text" name="password" id="password" /> <br/><br/>
+     
+         <button onClick = {this.loggaInHandler}>Logga in</button>
+       </div>)
+       }
+}
+
+export default Logga;
